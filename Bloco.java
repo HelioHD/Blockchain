f55a4e14518dfe1fdc48ce005c7a2d4916996e01
HelioHD <40152091+HelioHD@users.noreply.github.com>
2022-05-31 21:57:57 -0300

    Create Bloco.java

---
+++ Bloco.java
@@ -0,0 +1,10 @@
+
+
+import java.util.ArrayList;
+
+public class Bloco {
+	private String hashDoAnterior = "";
+	private String hashDoAtual = "";
+	private int nonce = 0;
+	private ArrayList<String> transacoes = new ArrayList<String>();
+}
